e491df1772c351afde02a028d46421e951ed6959
justin_hson <51263008+justin-a30@users.noreply.github.com>
2024-11-10 23:10:32 +0700

    sfdlkjfsdl

--- app/src/main/java/com/david42069/dualboothelper/java/SlotBActivity.java
+++ app/src/main/java/com/david42069/dualboothelper/java/SlotBActivity.java
@@ -1,7 +1,15 @@
 package com.david42069.dualboothelper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import dev.oneuiproject.oneui.layout.ToolbarLayout;
+import dev.oneuiproject.oneui.utils.ActivityUtils;
 
 public class SlotBActivity extends AppCompatActivity {
 
@@ -10,5 +18,7 @@ public class SlotBActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slot_a);  // Create a layout file for this
         // Initialize any UI components or actions specific to Slot A
+        ToolbarLayout toolbarLayout = findViewById(R.id.toolbar_layout);
+        toolbarLayout.setNavigationButtonAsBack();
     }
 }
